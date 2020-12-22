dab22e52d6559736f52c137f4695bdae12d21506
Osaid Makhalfih <Osaid40@hotmail.com>
2020-12-22 12:39:03 +0200

    newSignup

--- apps/heaven_app/urls.py
+++ apps/heaven_app/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', views.root),
+    path('signup', views.signup),
 ]
--- apps/heaven_app/views.py
+++ apps/heaven_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def root(request):
-    return render(request, 'index.html')
+def signup(request):
+    return render(request, 'heaven_app/signup.html')
