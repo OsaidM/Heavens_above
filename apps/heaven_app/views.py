0ee48c084cb90aef535cbe06314841c0b369a584
Osaid Makhalfih <Osaid40@hotmail.com>
2020-12-22 16:48:27 +0200

    newSignup_v02

--- apps/heaven_app/views.py
+++ apps/heaven_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def root(request):
+    return render(request, 'heaven_app/index.html')
 def signup(request):
     return render(request, 'heaven_app/signup.html')
+
+def about(request):
+    return render(request, 'heaven_app/about.html')
