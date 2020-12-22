358f24cce07fdd1639bbd14aa85ca4f53b1fcd01
Osaid Makhalfih <Osaid40@hotmail.com>
2020-12-22 18:42:54 +0200

    newHeaven_v03

--- apps/heaven_app/urls.py
+++ apps/heaven_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', views.root),
     path('signup', views.signup),
+    path('details', views.details),
     path('about', views.about),
     path('contact', views.contact),
     path('account', views.account),
--- apps/heaven_app/views.py
+++ apps/heaven_app/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def root(request):
     return render(request, 'heaven_app/index.html')
+
+def details(request):
+    return render(request, 'heaven_app/details.html')
+
 def signup(request):
     return render(request, 'heaven_app/signup.html')
 
